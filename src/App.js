9eecd121e0cf95bdc280b96cf0250752417ae286
Kathleen Hill <kath.hill@sbcglobal.net>
2020-02-09 22:28:15 +0000

    Completes Exercise 1 for Controlled Components

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    value: ''
+  };
+
+  updateValue = event => {
+    this.setState({ value: event.target.value });    
+  };
+
+  render() {
+    const {value} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input 
+			type="text" 
+			placeholder="Say Something"
+			value={value}
+			onChange={this.updateValue}
+			/>
+          <p className="echo">Echo:</p>
+          <p>{value}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
